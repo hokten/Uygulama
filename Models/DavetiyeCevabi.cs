6e1fbd2abad5294dcc1d2068cad1de90a94c4633
hokten <hhoohh2001@yahoo.com>
2024-03-13 09:30:17 +0200

    Create DavetiyeCevabi.cs
    DavetiyeCevabi modeli eklendi.

---
+++ Models/DavetiyeCevabi.cs
@@ -0,0 +1,10 @@
+namespace Uygulama.Models;
+
+public class DavetiyeCevabi
+{
+	public string? AdiSoyadi { get; set; }
+	public string? EPosta { get; set; }
+	public string? Telefon { get; set; }
+	public bool? KatilimDurumu { get; set; }
+
+}
